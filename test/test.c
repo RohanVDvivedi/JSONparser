420f3573339dd4c2b4a1ebcbcc73dceda3f07feb
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-11-02 14:01:55 +0530

    test modified to check for all the seg faults in the parser

--- test/test.c
+++ test/test.c
@@ -2,11 +2,32 @@
 
 int main()
 {
-	json_node* jnodep = parse_json(get_dstring("", 10));
+	printf("json string building start\n\n");
+
+	dstring* json_str = get_dstring("\
+	{ \
+	} \
+	", 10);
+
+	printf("String built : %s\n\n", json_str->cstring);
+
+	json_node* jnodep = parse_json(json_str);
+
+	printf("Parsing done\n\n");
+	
 	print_json_node(jnodep);
+
+	printf("Printing done\n\n");
+
 	if(jnodep != NULL)
 	{
-		delete_json_node(jnodep);;
+		delete_json_node(jnodep);
+
+		printf("Json node deleted\n\n");
 	}
+
+	delete_dstring(json_str);
+
+	printf("deletion of dstring\n\n");
 	return 0;
 }
