11fe5f19f49d78715c42d7118bf7e4ff85209275
Rohan Dvivedi <rohandvivedi@gmail.com>
2019-11-03 13:23:11 +0530

    corrected make file

--- inc/json_data_type.h
+++ inc/json_data_type.h
@@ -8,10 +8,11 @@ enum json_data_type
 	BOOLE = 1,		// dstring  (true, false)
 	NUMBER = 2,		// dstring  ([-][0-9][.][0-9])
 	STRING = 3,		// dstring  (*)
-	ARRAY = 4,		// array    (standard cutlery array of json_node*)
-	OBJECT = 5,		// hashmap  (standard cutlery hashmap of json_node*(STRING mandatorily) vs json_node*)
+	KEY = 4,		// dstring  (*) same as STRING, but this can only be inside an object, and map to any other data type
+	ARRAY = 5,		// array    (standard cutlery array of json_node*)
+	OBJECT = 6,		// hashmap  (standard cutlery hashmap of json_node*(KEY mandatorily) vs json_node*)
 
-	ERROR = 6		// this is when the json string has error and is not valid and can not be parsed
+	ERROR = 7		// this is when the json string has error and is not valid and can not be parsed
 					// ERROR dstring (*)
 };
 
