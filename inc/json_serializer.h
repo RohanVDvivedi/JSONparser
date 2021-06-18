9a67d22e23174d4337cdf83dc7ae1774093997c1
rohan <rohandvivedi@gmail.com>
2021-06-18 21:38:44 +0530

    removed the redundant comment

--- inc/json_serializer.h
+++ inc/json_serializer.h
@@ -2,10 +2,8 @@
 #define JSON_SERIALIZER
 
 #include<dstring.h>
-
 #include<json_node.h>
 
-// returns -1 if an ERROR is encountered in the json node, else 0 is returned
-int serialize_json(dstring* result, const json_node* node_p);
+void serialize_json(dstring* result, const json_node* node_p);
 
 #endif
