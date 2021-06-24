17342bac23b2a3b55ca8c317f7731679adfbb27c
rohan <rohandvivedi@gmail.com>
2021-06-24 11:36:09 +0530

    added lexing funcion for single character lexemes

--- src/json_lexer.c
+++ src/json_lexer.c
@@ -1,9 +1,79 @@
 #include<json_lexer.h>
 
+#include<ctype.h>
+
 void init_json_lexer(json_lexer* json_lexer_p, dstring* json_string)
 {
 	json_lexer_p->next_token_start = json_string->cstring;
 	json_lexer_p->json_string = json_string;
 }
 
-int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* lexeme_p);
+int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* json_lexeme_p)
+{
+	char* end_char_at = json_lexer_p->json_string->cstring + json_lexer_p->json_string->bytes_occupied;
+
+	if(json_lexer_p->next_token_start == end_char_at)
+	{
+		json_lexeme_p->type = END_OF_JSON_STRING;
+		return 1;
+	}
+
+	while((json_lexer_p->next_token_start != end_char_at) && isspace(*(json_lexer_p->next_token_start)))
+		json_lexer_p->next_token_start++;
+
+	if(json_lexer_p->next_token_start == end_char_at)
+	{
+		json_lexeme_p->type = END_OF_JSON_STRING;
+		return 1;
+	}
+
+	switch(*(json_lexer_p->next_token_start))
+	{
+		case '\"':
+		{
+			break;
+		}
+		case '{':
+		{
+			json_lexeme_p->type = OPEN_CURLY_BRACKET;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+		case '}':
+		{
+			json_lexeme_p->type = CLOSE_CURLY_BRACKET;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+		case '[':
+		{
+			json_lexeme_p->type = OPEN_SQUARE_BRACKET;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+		case ']':
+		{
+			json_lexeme_p->type = CLOSE_SQUARE_BRACKET;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+		case ':':
+		{
+			json_lexeme_p->type = COLON;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+		case ',':
+		{
+			json_lexeme_p->type = COMMA;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+			json_lexeme_p->value.bytes_occupied = 1;
+			return 1;
+		}
+	}
+}
