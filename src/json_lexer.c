97f2ad4be90b959fcfb223156fe8bb7617fba4c1
rohan <rohandvivedi@gmail.com>
2021-06-24 12:05:06 +0530

    added lexing for true false and null strings

--- src/json_lexer.c
+++ src/json_lexer.c
@@ -81,5 +81,71 @@ int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* json_lexeme_p)
 			json_lexeme_p->value.bytes_occupied = 1;
 			return 1;
 		}
+		case 'n':
+		{
+			static char* null_string = "null";
+			unsigned int null_string_length = 4;
+			json_lexeme_p->type = NULL_STRING;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
+			json_lexeme_p->value.bytes_occupied = null_string_length;
+			unsigned int seen = 0;
+			int is_null_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < null_string_length))
+			{
+				if((*(json_lexer_p->next_token_start++)) != null_string[seen++])
+				{
+					is_null_string = 0;
+					break;
+				}
+			}
+			if(is_null_string && seen == null_string_length)
+				return 1;
+			else
+				return 0;
+		}
+		case 't':
+		{
+			static char* true_string = "true";
+			unsigned int true_string_length = 4;
+			json_lexeme_p->type = TRUE_STRING;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
+			json_lexeme_p->value.bytes_occupied = true_string_length;
+			unsigned int seen = 0;
+			int is_true_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < true_string_length))
+			{
+				if((*(json_lexer_p->next_token_start++)) != true_string[seen++])
+				{
+					is_true_string = 0;
+					break;
+				}
+			}
+			if(is_true_string && seen == true_string_length)
+				return 1;
+			else
+				return 0;
+		}
+		case 'f':
+		{
+			static char* false_string = "false";
+			unsigned int false_string_length = 5;
+			json_lexeme_p->type = FALSE_STRING;
+			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
+			json_lexeme_p->value.bytes_occupied = false_string_length;
+			unsigned int seen = 0;
+			int is_false_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < false_string_length))
+			{
+				if((*(json_lexer_p->next_token_start++)) != false_string[seen++])
+				{
+					is_false_string = 0;
+					break;
+				}
+			}
+			if(is_false_string && seen == false_string_length)
+				return 1;
+			else
+				return 0;
+		}
 	}
 }
