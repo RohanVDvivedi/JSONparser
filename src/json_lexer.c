0455e1d0452b698771b2a8b3158e7b79d3ffc4e1
rohan <rohandvivedi@gmail.com>
2021-06-25 20:48:06 +0530

    added code to identify white spaces token lexeme

--- src/json_lexer.c
+++ src/json_lexer.c
@@ -18,15 +18,6 @@ int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* json_lexeme_p)
 		return 1;
 	}
 
-	while((json_lexer_p->next_token_start != end_char_at) && isspace(*(json_lexer_p->next_token_start)))
-		json_lexer_p->next_token_start++;
-
-	if(json_lexer_p->next_token_start == end_char_at)
-	{
-		json_lexeme_p->type = END_OF_JSON_STRING;
-		return 1;
-	}
-
 	switch(*(json_lexer_p->next_token_start))
 	{
 		case '\"':
@@ -175,6 +166,18 @@ int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* json_lexeme_p)
 				}
 
 				json_lexeme_p->value.bytes_occupied = json_lexer_p->next_token_start - json_lexeme_p->value.cstring;
+				return 1;
+			}
+			else if(isspace(*(json_lexer_p->next_token_start)))
+			{
+				json_lexeme_p->type = WHITESPACES_STRING;
+				json_lexeme_p->value.cstring = json_lexer_p->next_token_start++;
+				json_lexeme_p->value.bytes_occupied = 1;
+				while((json_lexer_p->next_token_start != end_char_at) && isspace(*(json_lexer_p->next_token_start)))
+				{
+					json_lexer_p->next_token_start++;
+					json_lexeme_p->value.bytes_occupied++;
+				}
 				return 1;
 			}
 			else
