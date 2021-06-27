c91ada80936d4c2dff2ba51a6954ce75696a29b7
rohan <rohandvivedi@gmail.com>
2021-06-27 10:35:41 +0530

    using dstring to represent static dstrings null true and fale

--- src/json_lexer.c
+++ src/json_lexer.c
@@ -76,66 +76,63 @@ int get_next_json_lexeme(json_lexer* json_lexer_p, json_lexeme* json_lexeme_p)
 		}
 		case 'n':
 		{
-			static char* null_string = "null";
-			unsigned int null_string_length = 4;
+			static dstring find_string = {.cstring="null", .bytes_occupied = 4, .bytes_allocated = 0};
 			json_lexeme_p->type = NULL_STRING;
 			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
-			json_lexeme_p->value.bytes_occupied = null_string_length;
 			unsigned int seen = 0;
-			int is_null_string = 1;
-			while((json_lexer_p->next_token_start != end_char_at) && (seen < null_string_length))
+			int is_find_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < find_string.bytes_occupied))
 			{
-				if((*(json_lexer_p->next_token_start++)) != null_string[seen++])
+				if((*(json_lexer_p->next_token_start++)) != json_lexeme_p->value.cstring[seen++])
 				{
-					is_null_string = 0;
+					is_find_string = 0;
 					break;
 				}
 			}
-			if(is_null_string && seen == null_string_length)
+			json_lexeme_p->value.bytes_occupied = json_lexer_p->next_token_start - json_lexeme_p->value.cstring;
+			if(is_find_string && seen == find_string.bytes_occupied)
 				return 1;
 			else
 				return 0;
 		}
 		case 't':
 		{
-			static char* true_string = "true";
-			unsigned int true_string_length = 4;
+			static dstring find_string = {.cstring="true", .bytes_occupied = 4, .bytes_allocated = 0};
 			json_lexeme_p->type = TRUE_STRING;
 			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
-			json_lexeme_p->value.bytes_occupied = true_string_length;
 			unsigned int seen = 0;
-			int is_true_string = 1;
-			while((json_lexer_p->next_token_start != end_char_at) && (seen < true_string_length))
+			int is_find_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < find_string.bytes_occupied))
 			{
-				if((*(json_lexer_p->next_token_start++)) != true_string[seen++])
+				if((*(json_lexer_p->next_token_start++)) != json_lexeme_p->value.cstring[seen++])
 				{
-					is_true_string = 0;
+					is_find_string = 0;
 					break;
 				}
 			}
-			if(is_true_string && seen == true_string_length)
+			json_lexeme_p->value.bytes_occupied = json_lexer_p->next_token_start - json_lexeme_p->value.cstring;
+			if(is_find_string && seen == find_string.bytes_occupied)
 				return 1;
 			else
 				return 0;
 		}
 		case 'f':
 		{
-			static char* false_string = "false";
-			unsigned int false_string_length = 5;
+			static dstring find_string = {.cstring="false", .bytes_occupied = 5, .bytes_allocated = 0};
 			json_lexeme_p->type = FALSE_STRING;
 			json_lexeme_p->value.cstring = json_lexer_p->next_token_start;
-			json_lexeme_p->value.bytes_occupied = false_string_length;
 			unsigned int seen = 0;
-			int is_false_string = 1;
-			while((json_lexer_p->next_token_start != end_char_at) && (seen < false_string_length))
+			int is_find_string = 1;
+			while((json_lexer_p->next_token_start != end_char_at) && (seen < find_string.bytes_occupied))
 			{
-				if((*(json_lexer_p->next_token_start++)) != false_string[seen++])
+				if((*(json_lexer_p->next_token_start++)) != json_lexeme_p->value.cstring[seen++])
 				{
-					is_false_string = 0;
+					is_find_string = 0;
 					break;
 				}
 			}
-			if(is_false_string && seen == false_string_length)
+			json_lexeme_p->value.bytes_occupied = json_lexer_p->next_token_start - json_lexeme_p->value.cstring;
+			if(is_find_string && seen == find_string.bytes_occupied)
 				return 1;
 			else
 				return 0;
