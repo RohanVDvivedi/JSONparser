4f9c2ab2c1a4f41b01da1d00f05f73bb46a67a2b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-11 03:51:03 -0700

    started implementing json node

--- inc/json_node.h
+++ inc/json_node.h
@@ -34,7 +34,11 @@ struct json_node
 		json_num json_number;
 		dstring json_string;
 		hashmap json_object;
-		array json_array;
+		struct
+		{
+			cy_uint json_array_size;
+			array json_array;
+		};
 	};
 };
 
@@ -56,11 +60,17 @@ json_node* new_json_string_node(const dstring* json_string);
 json_node* new_json_object_node(cy_uint element_count, const json_object_entry entries[]);
 json_node* new_json_array_node(cy_uint element_count, const json_node* elements[]);
 
+// append a node_p to array_node_p
+int append_to_json_array(json_node* array_node_p, const json_node* node_p);
+
+// remove an element at index from array_node_p and deletes it
+int delete_from_jsom_array(json_node* array_node_p, cy_uint index);
+
 // creates a new json_object_entry and inserts it into the json_object
 int insert_in_json_object(json_node* object_node_p, const dstring* key, const json_node* node_p);
 
-// removes a json_object_entry and deletes it
-int remove_from_json_object(json_node* object_node_p, json_object_entry* entry_p);
+// removes a json_object_entry from object_node_p and deletes it
+int delete_from_json_object(json_node* object_node_p, json_object_entry* entry_p);
 
 void delete_json_node(json_node* node_p);
 
