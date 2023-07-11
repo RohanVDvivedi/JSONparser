2b7dc48310b586d757974828632b006b5144db78
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-11 08:12:13 -0700

    json serializer implemented

--- src/json_serializer.c
+++ src/json_serializer.c
@@ -8,7 +8,7 @@ int serialize_json(stream* ws, const json_node* node_p)
 
 	if(node_p == NULL)
 	{
-		write_to_stream_formatted(ws, &error, "%s", "null");
+		write_to_stream_formatted(ws, &error, "null");
 		if(error)
 			return -1;
 		return 0;
@@ -42,10 +42,31 @@ int serialize_json(stream* ws, const json_node* node_p)
 		}
 		case JSON_OBJECT:
 		{
+			int is_first = 1;
+			for(const json_object_entry* e = get_first_of_in_hashmap(&(node_p->json_object), ANY_IN_HASHMAP); e != NULL; e = get_next_of_in_hashmap(&(node_p->json_object), e, ANY_IN_HASHMAP))
+			{
+				if(!is_first)
+					write_to_stream_formatted(ws, &error, ",");
+				write_to_stream_formatted(ws, &error, "\""printf_dstring_format"\":", printf_dstring_params(&(e->key)));
+				if(error)
+					return -1;
+				if(serialize_json(ws, e->value))
+					return -1;
+				is_first = 0;
+			}
 			break;
 		}
 		case JSON_ARRAY:
 		{
+			int is_first = 1;
+			for(cy_uint i = 0; i < get_element_count_arraylist(&(node_p->json_array)); i++)
+			{
+				if(!is_first)
+					write_to_stream_formatted(ws, &error, ",");
+				if(serialize_json(ws, get_nth_from_front_of_arraylist(&(node_p->json_array), i)))
+					return -1;
+				is_first = 0;
+			}
 			break;
 		}
 	}
