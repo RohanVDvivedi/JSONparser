c5e7a36ffc01b9283bf14307a2f33129f38be7e3
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-11 05:11:46 -0700

    implemented new function for json array and json object

--- src/json_node.c
+++ src/json_node.c
@@ -49,19 +49,44 @@ json_node* new_json_string_node(const dstring* string_value)
 	return n;
 }
 
-json_node* new_json_object_node(cy_uint element_count, const json_object_entry entries[]);
-json_node* new_json_array_node(cy_uint element_count, const json_node* elements[]);
+static cy_uint hash_json_object_entry(const void* ent_p)
+{
+	const json_object_entry* e = ent_p;
+	cy_uint hash = 0;
+	const char* key_data = get_byte_array_dstring(&(e->key));
+	cy_uint key_size = get_char_count_dstring(&(e->key));
+	for(cy_uint i = 0; i < key_size; i++)
+		hash = hash ^ (CY_UINT_C(13) * (i + 1) * ((cy_uint)key_data[i]));
+	return hash;
+}
 
-// append a node_p to array_node_p
+json_node* new_json_object_node(cy_uint element_count, const json_object_entry entries[])
+{
+	json_node* n = malloc(sizeof(json_node));
+	n->type = JSON_OBJECT;
+	initialize_hashmap(&(n->json_object), ELEMENTS_AS_LINKEDLIST_INSERT_AT_TAIL, ((element_count / 2) + 4), hash_json_object_entry, (int (*)(const void*, const void*))compare_dstring, offsetof(json_object_entry, embed_node));
+	for(cy_uint i = 0; i < element_count; i++)
+		insert_in_json_object(n, &(entries[i].key), entries[i].value);
+	return n;
+}
+
+json_node* new_json_array_node(cy_uint element_count, const json_node* elements[])
+{
+	json_node* n = malloc(sizeof(json_node));
+	n->type = JSON_ARRAY;
+	n->json_array_size = 0;
+	initialize_array(&(n->json_array), element_count);
+	for(cy_uint i = 0; i < element_count; i++)
+		append_to_json_array(n, elements[i]);
+	return n;
+}
+
 int append_to_json_array(json_node* array_node_p, const json_node* node_p);
 
-// remove an element at index from array_node_p and deletes it
 int delete_from_jsom_array(json_node* array_node_p, cy_uint index);
 
-// creates a new json_object_entry and inserts it into the json_object
 int insert_in_json_object(json_node* object_node_p, const dstring* key, const json_node* node_p);
 
-// removes a json_object_entry from object_node_p and deletes it
 int delete_from_json_object(json_node* object_node_p, json_object_entry* entry_p);
 
 void delete_json_node(json_node* node_p);
