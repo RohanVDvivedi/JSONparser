d0f18d0e809ae591e30d3439ac6913fa98e9e519
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-16 04:41:10 -0700

    started implementing top down parser

--- src/json_parser.c
+++ src/json_parser.c
@@ -4,90 +4,120 @@
 
 #include<stdio.h>
 
+static json_node* parse_json_node(lexer* lxr)
+{
+	// TODO
+}
+
+static json_node* parse_json_array_node(lexer* lxr)
+{
+	// TODO
+}
+
+static json_node* parse_json_object_node(lexer* lxr)
+{
+	json_node* js = NULL;
+
+	lexeme lxm;
+
+	// read the starting of the json object, a '{'
+	if(!get_next_lexeme_from_lexer(lxr, &lxm))
+		goto FAIL;
+	destroy_lexeme(&lxm);
+	if(lxm.type != CURLY_OPEN_BRACE)
+		goto FAIL;
+
+	// initialize a new json_object node
+	js = new_json_object_node(0, NULL);
+
+	// empty object case if next lexeme is a '}'
+	if(!get_next_lexeme_from_lexer(lxr, &lxm))
+		goto FAIL;
+	if(lxm.type == CURLY_CLOSE_BRACE)
+	{
+		destroy_lexeme(&lxm);
+		goto EXIT;
+	}
+	else
+		undo_lexer(lxr, &lxm);
+
+	while(1)
+	{
+		// read a string key
+		if(!get_next_lexeme_from_lexer(lxr, &lxm))
+			goto FAIL;
+		if(lxm.type != STRING_LEXEME)
+		{
+			destroy_lexeme(&lxm);
+			goto FAIL;
+		}
+		dstring key;
+		init_copy_dstring(&key, &(lxm.lexeme_str));
+		destroy_lexeme(&lxm);
+
+		// after key comes a ':'
+		if(!get_next_lexeme_from_lexer(lxr, &lxm))
+			goto FAIL;
+		destroy_lexeme(&lxm);
+		if(lxm.type != COLON)
+			goto FAIL;
+
+		// parse value
+		json_node* value = parse_json_node(lxr);
+
+		// insert parsed json key value into the json object
+		insert_in_json_object(js, &key, value);
+
+		// after key comes a ',' or a '}'
+		if(!get_next_lexeme_from_lexer(lxr, &lxm))
+			goto FAIL;
+		destroy_lexeme(&lxm);
+		if(lxm.type == COMMA)
+			continue;
+		else if(lxm.type == CURLY_CLOSE_BRACE)
+			goto EXIT;
+		else
+			goto FAIL;
+	}
+
+	FAIL:;
+	delete_json_node(js);
+	js = NULL;
+
+	EXIT:;
+	return js;
+}
+
 json_node* parse_json(stream* rs, size_t max_json_string_length, size_t max_json_number_length)
 {
 	lexer lxr;
 	initialize_lexer(&lxr, rs, max_json_string_length, max_json_number_length);
 
 	lexeme lxm;
-	while(1)
+
+	json_node* js = NULL;
+
+	if(!get_next_lexeme_from_lexer(&lxr, &lxm))
+		goto EXIT;
+
+	if(lxm.type == CURLY_OPEN_BRACE)
 	{
-		if(!get_next_lexeme_from_lexer(&lxr, &lxm))
-		{
-			printf("ERROR\n");
-			break;
-		}
-
-		switch(lxm.type)
-		{
-			case END_OF_STREAM:
-			{
-				printf("END_OF_STREAM\n");
-				break;
-			}
-			case CURLY_OPEN_BRACE :
-			{
-				printf("CURLY_OPEN_BRACE\n");
-				break;
-			}
-			case CURLY_CLOSE_BRACE :
-			{
-				printf("CURLY_CLOSE_BRACE\n");
-				break;
-			}
-			case SQUARE_OPEN_BRACE :
-			{
-				printf("SQUARE_OPEN_BRACE\n");
-				break;
-			}
-			case SQUARE_CLOSE_BRACE :
-			{
-				printf("SQUARE_CLOSE_BRACE\n");
-				break;
-			}
-			case COMMA :
-			{
-				printf("COMMA\n");
-				break;
-			}
-			case COLON :
-			{
-				printf("COLON\n");
-				break;
-			}
-			case TRUE :
-			{
-				printf("TRUE\n");
-				break;
-			}
-			case FALSE :
-			{
-				printf("FALSE\n");
-				break;
-			}
-			case NULL_LEXEME :
-			{
-				printf("NULL_LEXEME\n");
-				break;
-			}
-			case STRING_LEXEME :
-			{
-				printf("STRING_LEXEME : <"printf_dstring_format">\n", printf_dstring_params(&(lxm.lexeme_str)));
-				break;
-			}
-			case NUMBER_LEXEME :
-			{
-				printf("NUMBER_LEXEME : <"printf_dstring_format">\n", printf_dstring_params(&(lxm.lexeme_str)));
-				break;
-			}
-		}
-
+		undo_lexer(&lxr, &lxm);
+		js = parse_json_object_node(&lxr);
+	}
+	else if(lxm.type == SQUARE_OPEN_BRACE)
+	{
+		undo_lexer(&lxr, &lxm);
+		js = parse_json_array_node(&lxr);
+	}
+	else
+	{
 		destroy_lexeme(&lxm);
-
-		if(lxm.type == END_OF_STREAM)
-			break;
+		goto EXIT;
 	}
 
+
+	EXIT:;
 	deinitialize_lexer(&lxr);
-	return NULL;
+	return js;
 }
