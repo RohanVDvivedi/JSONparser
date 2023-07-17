836b59a578e36ebf230fa6fbb492726613447277
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-17 06:04:31 -0700

    added more error code for lexer

--- inc/json_parser.h
+++ inc/json_parser.h
@@ -6,11 +6,12 @@
 #include<json_node.h>
 
 #define NO_ERROR           0
-#define PARSE_ERROR        1
-#define STREAM_ERROR       2
-#define ALLOCATION_ERROR   3
+#define LEXER_ERROR        1
+#define PARSE_ERROR        2
+#define STREAM_ERROR       3
+#define ALLOCATION_ERROR   4
 
-// returns NULL on error
+// returns NULL on error if error is != 0
 json_node* parse_json(stream* rs, size_t max_json_string_length, size_t max_json_number_length);
 
 #endif
