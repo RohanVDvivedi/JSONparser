d944bd384f0c89210e3702bcfef332e089b191c7
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-16 22:31:55 -0700

    added error macros

--- inc/json_parser.h
+++ inc/json_parser.h
@@ -5,6 +5,11 @@
 
 #include<json_node.h>
 
+#define NO_ERROR           0
+#define PARSE_ERROR        1
+#define STREAM_ERROR       2
+#define ALLOCATION_ERROR   3
+
 // returns NULL on error
 json_node* parse_json(stream* rs, size_t max_json_string_length, size_t max_json_number_length);
 
