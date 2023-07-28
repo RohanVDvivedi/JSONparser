241e05f6d00f85eb8dc55a6de8cc8f1a68dff41b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-28 08:33:53 -0700

    added global header file for maintaining error codes

--- inc/json_parser.h
+++ inc/json_parser.h
@@ -5,11 +5,7 @@
 
 #include<json_node.h>
 
-#define JSON_NO_ERROR          0
-#define JSON_LEXER_ERROR      -1
-#define JSON_PARSER_ERROR     -2
-#define JSON_ERROR_IN_STREAM  -3
-#define JSON_ALLOCATION_ERROR -4
+#include<json_parser_error_codes.h>
 
 // returns error if error is != 0, then returned value is going to be NULL
 json_node* parse_json(stream* rs, size_t max_json_string_length, size_t max_json_number_length, int* error);
---
+++ inc/json_parser_error_codes.h
@@ -0,0 +1,10 @@
+#ifndef JSON_PARSER_ERROR_CODES_H
+#define JSON_PARSER_ERROR_CODES_H
+
+#define JSON_NO_ERROR          0
+#define JSON_LEXER_ERROR      -1
+#define JSON_PARSER_ERROR     -2
+#define JSON_ERROR_IN_STREAM  -3
+#define JSON_ALLOCATION_ERROR -4
+
+#endif
