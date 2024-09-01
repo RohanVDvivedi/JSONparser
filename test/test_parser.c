9f493411c964fb0b2b4efcefe26a57679a62ca81
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-01 19:58:43 +0530

    refactored test cases to use the new headers from cutlery instead of the old ones from ConnMan

--- test/test_parser.c
+++ test/test_parser.c
@@ -4,7 +4,7 @@
 #include<json_parser.h>
 #include<json_serializer.h>
 
-#include<file_descriptor_stream.h>
+#include<stream_for_file_descriptor.h>
 
 int main()
 {
--- test/test_serializer.c
+++ test/test_serializer.c
@@ -5,7 +5,7 @@
 #include<json_serializer.h>
 
 #include<stream_util.h>
-#include<file_descriptor_stream.h>
+#include<stream_for_file_descriptor.h>
 
 #define dst(x) get_dstring_pointing_to_literal_cstring(x)
 
