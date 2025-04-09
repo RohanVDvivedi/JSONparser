e14a5e1ce9ac4fc2d27eb49e958e3d49aed9b494
RohanVDvivedi <rohandvivedi@gmail.com>
2025-04-09 10:34:57 +0530

    major bug fix

--- src/json_accessor.c
+++ src/json_accessor.c
@@ -8,7 +8,7 @@ json_node* get_json_node_from_json_node(json_node* node_p, json_accessor acs)
 
 		if(k.is_array_index && node_p->type == JSON_ARRAY)
 			node_p = fetch_json_node_from_json_array(node_p, k.index);
-		else if((!k.is_array_index) && node_p->type == JSON_ARRAY)
+		else if((!k.is_array_index) && node_p->type == JSON_OBJECT)
 			node_p = fetch_json_node_from_json_object(node_p, &(k.key));
 		else
 			return NULL;
